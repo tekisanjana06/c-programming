6019566f4e6199ca2f53e660db147642d0d051df
c <tekisanjana06@gmail.com>
2025-12-05 10:09:34 +0530

    Add files via upload

---
+++ multiplication.c-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= 12; i++) {
+        printf("%d x %d = %d
+", N, i, N * i);
+    }
+
+    return 0;
+}
+
