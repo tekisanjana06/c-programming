193a494488dfb2fd1a41d8d50631f8a63bc537b4
c <tekisanjana06@gmail.com>
2025-12-05 15:53:05 +0530

    Add files via upload

---
+++ sentence to words (2).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char a[30];
+	scanf("%[^\n]s", a);
+	int i;
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		printf("\n");
+		else
+		printf("%c",a[i]);
+	}
+	return 0;
+}
+
