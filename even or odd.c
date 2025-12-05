6d27f8ef39c1ef3ae85bd3c66176cd88050f774c
c <tekisanjana06@gmail.com>
2025-12-05 10:33:38 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int X;
+
+    // Read the input number
+    scanf("%d", &X);
+
+    // Check if the number is even or odd
+    if (X % 2 == 0) {
+        printf("Even
+");
+    } else {
+        printf("Odd
+");
+    }
+
+    return 0;
+}
+
